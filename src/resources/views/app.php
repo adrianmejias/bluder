82017bcf517a23d9fd6712dc3131d2842b9bacac
Adrian Mejias <adrianmejias@gmail.com>
2021-12-14 03:49:25 -0600

    wip

--- src/resources/views/app.php
+++ src/resources/views/app.php
@@ -1,10 +1,10 @@
 <style type="text/css" media="screen">
-    <?php include_once dirname(__DIR__, 1) . '/css/app.css'; ?>
+    <?php readfile(dirname(__DIR__, 1) . '/css/app.css'); ?>
 </style>
 <div class="w-full p-5 text-white absolute top-0 bg-gray-700">
     <div class="flex justify-between align-middle">
         <div class="font-bold mb-5 text-gray-400">
-            <a href="https://stackoverflow.com/search?q=[php]+<?php echo str_replace(' ', '+', \AdrianMejias\Blunder\Blunder::entities($this->stackTrace['message'])); ?>" target="_blank" rel="nofollow">
+            <a href="https://stackoverflow.com/search?q=[php]+<?php echo str_replace(' ', '+', htmlentities($this->stackTrace['message'], ENT_QUOTES, 'utf-8', false)); ?>" target="_blank" rel="nofollow">
                 <div class="inline-block">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round" class="w-4 h-4">
                         <path d="M0 0h24v24H0z" stroke="none"></path>
@@ -48,7 +48,7 @@
                                 [ <?php echo $key; ?> ] =&gt;
                             </div>
                             <div class="ml-1 text-gray-300 truncate overflow-hidden">
-                                <?php echo is_string($value) ? \AdrianMejias\Blunder\Blunder::entities($value) : print_r($value, true); ?>
+                                <?php echo is_string($value) ? htmlentities($value, ENT_QUOTES, 'utf-8', false) : print_r($value, true); ?>
                             </div>
                         </div>
                     <?php endforeach; ?>
